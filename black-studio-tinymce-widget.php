f98207d691a9de80f7df300f29030bb78841e986
marcochiesi <marcochiesi@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-11-18 21:58:56 +0000

    * Fixed paragraph formatting bug on saving * Fixed real-time update bug in Theme Customizer * Enhanced editor initialization * Simplified internal integration with Page Builder * Simplified internal initialization for accessibility mode * Minor changes for coding standard compliance
    git-svn-id: https://plugins.svn.wordpress.org/black-studio-tinymce-widget/trunk@1028287 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- black-studio-tinymce-widget.php
+++ black-studio-tinymce-widget.php
@@ -3,7 +3,7 @@
 Plugin Name: Black Studio TinyMCE Widget
 Plugin URI: https://wordpress.org/plugins/black-studio-tinymce-widget/
 Description: Adds a new "Visual Editor" widget type based on the native WordPress TinyMCE editor.
-Version: 2.2.0
+Version: 2.2.1
 Author: Black Studio
 Author URI: http://www.blackstudio.it
 Requires at least: 3.1
@@ -35,7 +35,7 @@ if ( ! class_exists( 'Black_Studio_TinyMCE_Plugin' ) ) {
 		 * @var string
 		 * @since 2.0.0
 		 */
-		public static $version = '2.2.0';
+		public static $version = '2.2.1';
 
 		/**
 		 * The single instance of the plugin class
@@ -265,7 +265,7 @@ else {
 
 	/* Check for multiple plugin instances */
 	if ( ! function_exists( 'bstw_multiple_notice' ) ) {
-	
+
 		/**
 		 * Show admin notice when multiple instances of the plugin are detected
 		 *
@@ -282,7 +282,7 @@ else {
 			}
 		}
 		add_action( 'admin_notices', 'bstw_multiple_notice' );
-		
+	
 	} // END function_exists bstw_multiple_notice check
 
 } // END else function_exists bstw check
